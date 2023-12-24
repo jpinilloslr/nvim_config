65e0d33f2a0e73c1690d96f806d0f6b0979d65e2
Joaquin Pinillos <jpinilloslr@gmail.com>
2023-12-24 14:38:12 +0100

    Change nvim-tree config

--- init.lua
+++ init.lua
@@ -222,9 +222,9 @@ require('lazy').setup({
     },
     config = function()
       require("nvim-tree").setup {
-        view = {
-          float = {
-            enable = true
+        actions = {
+          open_file = {
+            quit_on_open = true
           }
         }
       }
