314ea21ee26adfe2f1639c34975909ddeb7d8188
Joaquin Pinillos <jpinilloslr@gmail.com>
2023-12-27 00:11:16 +0100

    Load autocmds

--- init.lua
+++ init.lua
@@ -20,3 +20,4 @@ require("lazy").setup("plugins")
 
 require("config.options");
 require("config.keymaps");
+require("config.autocmds");
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -22,21 +22,18 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     }
 
     if vim.tbl_contains(prettier_filetypes, filetype) then
-      -- Use Prettier for these filetypes
       local project_prettier = vim.fn.getcwd() .. "/node_modules/.bin/prettier"
       local prettier_executable = vim.fn.executable(project_prettier) == 1 and project_prettier or "prettier"
       local prettier_command = prettier_executable .. " --stdin-filepath " .. vim.fn.shellescape(vim.fn.expand("%"))
       local result = vim.fn.system(prettier_command, vim.api.nvim_buf_get_lines(bufnr, 0, -1, false))
       local success = vim.v.shell_error == 0
 
-      -- Replace buffer content only if Prettier was successful
       if success then
         local cursor_pos = vim.api.nvim_win_get_cursor(0)
         local lines = vim.split(vim.trim(result), "\n")
         vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
         pcall(vim.api.nvim_win_set_cursor, 0, cursor_pos)
       else
-        -- Print error if Prettier was not successful
         print("Prettier formatting failed: " .. vim.fn.trim(result))
       end
     else
