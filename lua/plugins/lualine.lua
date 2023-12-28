b24d8ad4837e3a7c20407aff5d89326a171a2100
Joaquin Pinillos <jpinilloslr@gmail.com>
2023-12-28 01:34:31 +0100

    Change to lualine defaults

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,10 +2,7 @@ return {
   'nvim-lualine/lualine.nvim',
   opts = {
     options = {
-      icons_enabled = false,
       theme = 'tokyonight',
-      component_separators = '|',
-      section_separators = '',
     },
   },
 }
