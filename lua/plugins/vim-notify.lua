d9baee906eca426905f8595fa54777531f4a398f
Joaquin Pinillos <jpinilloslr@gmail.com>
2024-01-03 00:12:08 +0100

    Add vim-notify keymaps

--- lua/plugins/vim-notify.lua
+++ lua/plugins/vim-notify.lua
@@ -2,5 +2,13 @@ return {
   'rcarriga/nvim-notify',
   config = function()
     vim.notify = require('notify')
+
+    vim.keymap.set('n', '<leader>nh', function()
+      require('notify')._print_history()
+    end, { desc = '[N]otifications [H]istory' })
+
+    vim.keymap.set('n', '<leader>nd', function()
+      require('notify').dismiss({})
+    end, { desc = '[N]otifications [D]ismiss All' })
   end
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -7,6 +7,7 @@ return {
       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
       ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      ['<leader>n'] = { name = '[N]otifications', _ = 'which_key_ignore' },
       ['<leader>o'] = { name = 'T[o]ggle', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
