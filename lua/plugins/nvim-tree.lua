00ab762fa39598d1b8dedc9174273cd88d4c52ed
Joaquin Pinillos <jpinilloslr@gmail.com>
2024-01-06 20:13:46 +0100

    Activate update_focused_file on NvimTree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,6 +7,9 @@ return {
   },
   config = function()
     require('nvim-tree').setup {
+      update_focused_file = {
+        enable = true,
+      },
       actions = {
         open_file = {
           quit_on_open = true
