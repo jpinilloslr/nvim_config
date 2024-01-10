4466d03ad86eb72fa07faa2c1cf762af0b1d1a05
Joaquin Pinillos <jpinilloslr@gmail.com>
2024-01-10 11:39:35 +0100

    Change telescope to inlcude hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,6 +13,11 @@ return {
   },
   config = function()
     require('telescope').setup {
+      pickers = {
+        find_files = {
+          hidden = true
+        }
+      },
       defaults = {
         mappings = {
           i = {
