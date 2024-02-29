d04ceef04a368b5f4d3cbb57483e3303648734c9
Joaquin Pinillos <jpinilloslr@gmail.com>
2024-02-29 10:26:37 +0100

    Add default indentation config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -26,3 +26,36 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
+
+-- Set indentation config
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+local langs_indent_config = {
+  python = { tabstop = 4, shiftwidth = 4, expandtab = true },
+  javascript = { tabstop = 2, shiftwidth = 2, expandtab = true },
+  typescript = { tabstop = 2, shiftwidth = 2, expandtab = true },
+  lua = { tabstop = 2, shiftwidth = 2, expandtab = true },
+  go = { tabstop = 4, shiftwidth = 4, expandtab = false },
+  rust = { tabstop = 4, shiftwidth = 4, expandtab = true },
+  c = { tabstop = 4, shiftwidth = 4, expandtab = false },
+  cpp = { tabstop = 4, shiftwidth = 4, expandtab = false },
+  java = { tabstop = 4, shiftwidth = 4, expandtab = true },
+  html = { tabstop = 2, shiftwidth = 2, expandtab = true },
+  css = { tabstop = 2, shiftwidth = 2, expandtab = true },
+  yaml = { tabstop = 2, shiftwidth = 2, expandtab = true },
+  json = { tabstop = 2, shiftwidth = 2, expandtab = true },
+}
+
+-- Override defaults with specific language settings
+for lang, opts in pairs(langs_indent_config) do
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = lang,
+    callback = function()
+      vim.bo.tabstop = opts.tabstop
+      vim.bo.shiftwidth = opts.shiftwidth
+      vim.opt.expandtab = opts.expandtab
+    end,
+  })
+end
