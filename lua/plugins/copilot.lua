f62f8712561714846ca36f8a9a263d4e881aa289
Joaquin Pinillos <jpinilloslr@gmail.com>
2024-02-29 10:26:12 +0100

    Change copilot keymap

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,6 +2,6 @@ return {
   "github/copilot.vim",
   config = function()
     vim.g.copilot_assume_mapped = true
-    vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    vim.api.nvim_set_keymap("i", "<C-S>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
   end,
 }
