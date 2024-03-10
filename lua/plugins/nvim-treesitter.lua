d5f76c518a436d6f32cd316d50140c56aa4938e0
Joaquin Pinillos <jpinilloslr@gmail.com>
2024-03-10 18:52:19 +0100

    Add treesitter text objects.

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -54,51 +54,69 @@ return {
               ['ar'] = '@parameter.outer',
               ['ab'] = '@block.outer',
               ['ib'] = '@block.inner',
+              ['ak'] = '@assignment.lhs',
+              ['ik'] = '@assignment.lhs',
+              ['av'] = '@assignment.rhs',
+              ['iv'] = '@assignment.rhs',
             },
           },
           move = {
             enable = true,
             set_jumps = true,
             goto_next_start = {
+              [']a'] = '@parameter.inner',
+              [']b'] = '@block.inner',
               [']f'] = '@function.outer',
-              [']o'] = '@conditional.outer',
-              [']]'] = '@class.outer',
-              [']l'] = '@loop.*',
+              [']k'] = '@assignment.lhs',
+              [']v'] = '@assignment.rhs',
             },
             goto_next_end = {
+              [']A'] = '@parameter.inner',
+              [']B'] = '@block.inner',
               [']F'] = '@function.outer',
-              [']['] = '@class.outer',
+              [']K'] = '@assignment.lhs',
+              [']V'] = '@assignment.rhs',
             },
             goto_previous_start = {
+              ['[a'] = '@parameter.inner',
+              ['[b'] = '@block.inner',
               ['[f'] = '@function.outer',
-              ['[o'] = '@conditional.outer',
-              ['[['] = '@class.outer',
-              ['[l'] = '@loop.*',
+              ['[k'] = '@assignment.lhs',
+              ['[v'] = '@assignment.rhs',
             },
             goto_previous_end = {
+              ['[A'] = '@parameter.inner',
+              ['[B'] = '@block.inner',
               ['[F'] = '@function.outer',
-              ['[]'] = '@class.outer',
+              ['[K'] = '@assignment.lhs',
+              ['[V'] = '@assignment.rhs',
             },
-            goto_next = {
-              [']a'] = '@parameter.outer',
-              [']b'] = '@block.inner',
-            },
-            goto_previous = {
-              ['[a'] = '@parameter.outer',
-              ['[b'] = '@block.inner',
-            }
           },
           swap = {
             enable = true,
             swap_next = {
-              ['<leader>a'] = '@parameter.inner',
+              ['<leader>wa'] = '@parameter.inner',
+              ['<leader>wf'] = '@function.outer',
             },
             swap_previous = {
-              ['<leader>A'] = '@parameter.inner',
+              ['<leader>wA'] = '@parameter.inner',
+              ['<leader>wF'] = '@function.outer',
             },
           },
         },
       }
+
+      local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+
+      -- Repeat movement with ; and ,
+      vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+      vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+
+      -- Make builtin f, F, t, T also repeatable with ; and ,
+      vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+      vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+      vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+      vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
     end, 0)
   end
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -11,6 +11,7 @@ return {
       ['<leader>n'] = { name = '[N]otifications', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+      ['<leader>w'] = { name = 'S[w]ap', _ = 'which_key_ignore' },
     }
 
     -- gister which-key VISUAL mode
