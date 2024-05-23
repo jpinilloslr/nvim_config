894983ec05b8bef14a9daa7e408172cf49e7c243
joaquin <jpinilloslr@gmail.com>
2024-05-23 13:25:15 +0200

    feat(keymaps): add quick fix navigation shortcuts
    This commit adds two new keymaps for navigating through quick fixes in
    Neovim. The '[q' keymap is used to go to the previous quick fix, and
    the ']q' keymap is used to go to the next quick fix.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -30,3 +30,7 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Quick fix
+vim.keymap.set('n', '[q', ':cprevious<CR>', { desc = 'Go to previous quick fix' })
+vim.keymap.set('n', ']q', ':cnext<CR>', { desc = 'Go to next quick fix' })
