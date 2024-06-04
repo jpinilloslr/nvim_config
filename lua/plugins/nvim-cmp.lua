d25beeebaf693ef297234d0352042de3b867133f
joaquin <jpinilloslr@gmail.com>
2024-06-04 10:15:17 +0200

    feat(nvim-cmp): add vim-dadbod-completion for SQL filetype
    This commit introduces a new completion source for SQL files in
    nvim-cmp. The source is vim-dadbod-completion, which provides
    database-related completions. This source is added alongside the
    existing buffer source.
    
    In addition, this commit adds the vim-dadbod, vim-dadbod-ui, and
    vim-dadbod-completion plugins to the project.

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -63,5 +63,12 @@ return {
         { name = 'path' },
       },
     }
+
+    cmp.setup.filetype({ 'sql' }, {
+      sources = {
+        { name = 'vim-dadbod-completion' },
+        { name = 'buffer' },
+      }
+    })
   end
 }
