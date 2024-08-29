957935839a0b1dc349d359615521508b01d48ce3
joaquin <jpinilloslr@gmail.com>
2024-08-29 11:54:41 +0200

    chore: update which-key config to latest schema

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,25 +1,24 @@
 return {
   'folke/which-key.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'echasnovski/mini.nvim'
+  },
   config = function()
-    -- Document existing key chains
-    require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ebug', _ = 'which_key_ignore' },
-      ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      ['<leader>l'] = { name = '[L]SP', _ = 'which_key_ignore' },
-      ['<leader>n'] = { name = '[N]otifications', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      ['<leader>T'] = { name = '[T]ests', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = 'S[w]ap', _ = 'which_key_ignore' },
+    local wk = require('which-key')
+    wk.add({
+      { '<leader>c',  group = '[C]ode' },
+      { '<leader>d',  group = '[D]ebug' },
+      { '<leader>g',  group = '[G]it' },
+      { '<leader>h',  group = 'Git [H]unk', mode = 'n' },
+      { '<leader>h',  group = 'Git [H]unk', mode = 'v' },
+      { '<leader>l',  group = '[L]SP' },
+      { '<leader>n',  group = '[N]otifications' },
+      { '<leader>s',  group = '[S]earch' },
+      { '<leader>t',  group = '[T]oggle' },
+      { '<leader>T',  group = '[T]ests' },
+      { '<leader>w',  group = 'S[w]ap' },
     }
-
-    -- gister which-key VISUAL mode
-    -- required for visual <leader>hs (hunk stage) to work
-    require('which-key').register({
-      ['<leader>'] = { name = 'VISUAL <leader>' },
-      ['<leader>h'] = { 'Git [H]unk' },
-    }, { mode = 'v' })
+  )
   end
 }
